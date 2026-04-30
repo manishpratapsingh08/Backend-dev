12f7c8e3fed5503f084f281af63a91c38d42621b
manish <manishpratapsingh0804@gmail.com>
2026-04-30 11:20:37 +0530

    Add files via upload

---
+++ exercise5_event_loop_demo.js
@@ -0,0 +1,23 @@
+console.log('Start');
+
+setTimeout(() => {
+  console.log('setTimeout callback');
+}, 0);
+
+setImmediate(() => {
+  console.log('setImmediate callback');
+});
+
+process.nextTick(() => {
+  console.log('process.nextTick callback');
+});
+
+Promise.resolve()
+  .then(() => {
+    console.log('Promise.then callback');
+  })
+  .then(() => {
+    console.log('Promise second then callback');
+  });
+
+console.log('End');
